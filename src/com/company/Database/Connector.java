26e08a03bd204f0ea42e67a01f873fe3a3f2a403
mmowka <mateusz.mowka@intel.com>
2020-07-03 16:19:32 +0200

    Add connector and add some cars to the database.

--- src/com/company/Database/Connector.java
+++ src/com/company/Database/Connector.java
@@ -1,4 +1,27 @@
 package com.company.Database;
 
+import java.sql.*;
+import java.util.Properties;
+
 public class Connector {
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/CarMonopoly";
+    static final String USER = "postgres";
+    static final String PASS = "password123";
+    static private Connection CONNECTION;
+
+    public static void connect() throws SQLException, ClassNotFoundException {
+        Properties props = new Properties();
+        props.setProperty("user", USER);
+        props.setProperty("password", PASS);
+        CONNECTION = DriverManager.getConnection(DB_URL, props);
+        System.out.println("Connected");
+    }
+
+    public static Statement getStatement() throws SQLException {
+        return CONNECTION.createStatement();
+    }
+
+    public static void executeSQL(String sql) throws SQLException {
+        CONNECTION.createStatement().execute(sql);
+    }
 }
